6acfe9ff460dea01369f1995dc69e7d9d09f8958
mala0116 <maksim_larin@yahoo.com>
2017-01-27 02:57:26 +0300

    initial commit

--- src/main/java/com/gallows/Main.java
+++ src/main/java/com/gallows/Main.java
@@ -1,14 +1,12 @@
 package com.gallows;
 
-import com.gallows.models.Game;
 import com.gallows.helpers.GameStatus;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.gallows.models.Game;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.LogManager;
 
 public class Main {
 
